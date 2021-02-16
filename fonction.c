31331f1a038f6f551027bf0336515204f0dcf49b
Charles <renoit-charles@outlook.fr>
2021-02-16 15:51:08 +0100

    fonction rendu

--- fonction.c
+++ fonction.c
@@ -20,3 +20,29 @@ extern void SDL_ExitImageWithError(const char *message, void * renderer, void * 
   SDL_DestroyWindow(window);
   SDL_ExitWithFailure(message);
 }
+
+// Créer une texture
+extern void ajout_texture(SDL_Texture *texture ,const char * lien_img , SDL_Renderer *renderer, SDL_Window *window, int hauteur, int largeur){
+  SDL_Surface *image = NULL;
+  image = IMG_Load(lien_img);
+
+    if(image == NULL)
+        SDL_ExitImageWithError("L'image n'as pas été récupéré", renderer, window);
+
+    texture = SDL_CreateTextureFromSurface(renderer, image);
+
+    SDL_FreeSurface(image);
+
+    if(texture == NULL)
+        SDL_ExitImageWithError("La texture n'as pas été créé", renderer, window);
+
+    SDL_Rect dimension_image;
+    SDL_QueryTexture(texture, NULL, NULL, &dimension_image.w, &dimension_image.h);
+
+    dimension_image.x = (largeur - dimension_image.w) / 2;
+    dimension_image.y = (hauteur - dimension_image.h) / 2;
+
+    SDL_RenderCopy(renderer, texture, NULL, &dimension_image);
+  free(image);
+  image = NULL;
+}
