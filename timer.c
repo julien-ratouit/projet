d28b727057df87d18e772704da4cb8be8cdc83eb
Gugulicorne <guiguiv61.270400@gmail.com>
2021-03-08 15:48:23 +0100

    Timer le retour
    C'est le vrai

--- timer.c
+++ timer.c
@@ -35,29 +35,6 @@ typedef struct Timer_s{
 };Timer_t
 
 
-//Initialise le timer
-void timer_init(Timer_t * Timer){
-
-    //Création de l'espace du timer
-    Timer = malloc(sizeof(Timer_t));
-
-    //Initialisation de valeur de base
-    Timer->start = false;
-    Timer->paused = false;
-    Timer->tick_debut = 0;
-    Timer->tick_pause = 0;
-
-    //Initialisation des fonctions a utiliser
-    Timer->debut = timer_debut;
-    Timer->stop = timer_stop;
-    Timer->pause = timer_pause;
-    Timer->unpause = timer_unpause;
-    Timer->get_ticks = timer_get_ticks;
-    Timer->is_started = timer_is_started;
-    Timer->is_paused = timer_is_paused;
-
-}
-
 
 //Démarre le timer et initialise le début du timer 
 //(A utiliser 1 fois ! Pour enlever la pause faire unpause)
@@ -146,10 +123,27 @@ bool timer_is_paused(){
 
 }
 
-int main(){
+//Initialise le timer
+Timer_t * timer_init(){
 
-    Timer_t myTimer;
-    init_timer(&myTimer);
- 
-    
+    //Création de l'espace du timer    
+	Timer_t * Timer;
+	Timer = malloc(sizeof(Timer_t));
+
+    //Initialisation de valeur de base
+    Timer->start = false;
+    Timer->paused = false;
+    Timer->tick_debut = 0;
+    Timer->tick_pause = 0;
+
+    //Initialisation des fonctions a utiliser
+    Timer->debut = timer_debut;
+    Timer->stop = timer_stop;
+    Timer->pause = timer_pause;
+    Timer->unpause = timer_unpause;
+    Timer->get_ticks = timer_get_ticks;
+    Timer->is_started = timer_is_started;
+    Timer->is_paused = timer_is_paused;
+	
+	return Timer;
 }
