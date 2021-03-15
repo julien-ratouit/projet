5f7508e1634719ac109595bb7bb7c1845ac8dc0d
Maxime-Quenault <maximeq72@gmail.com>
2021-03-15 09:19:21 +0100

    realisation save.h
    j'ai mis à jour le save.h que guillaume avait oublié ;)

---
+++ save.h
@@ -0,0 +1,3 @@
+extern void sauvegarde(int money, int jour,int nb_action, int action_achete[], int action_equipe[]);
+extern int charger(int * money, int * jour, int * nb_action, int action_achete[], int action_equipe[]);
+extern int main();
