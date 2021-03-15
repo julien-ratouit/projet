5644f6e49790501547314c452d3a54787c859fd3
Gugulicorne <guiguiv61.270400@gmail.com>
2021-03-15 12:12:40 +0100

    Step up action.c
    PAs fini mais y a des commentaires

--- action.h
+++ action.h
@@ -8,6 +8,7 @@ typedef struct action_s
 	int cd;
 	//Savoir si l'action est acheté ou pas
 	bool statut;
+	int equipe;
 	//Fonction qui va être utilisé dans le jeu
 	void (*action_realise)(int); 
 }
@@ -15,4 +16,5 @@ action_t;
 
 extern action_t *init_action(int id, int prix, int cd, void (*fonction)(int));
 extern void init_tab_action(action_t * tab[]);
-extern int achat_action(action_t *action, int argent);
+extern int achat_action(action_t *action, int *argent);
+extern int equipe_action(int place, action_t * tab_equipe[4], action_t * tab_action[]);
