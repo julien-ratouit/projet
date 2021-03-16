a27c19ad6c843b287c89ad230de0874d81661cb2
julien-ratouit <ratouitj@gmail.com>
2021-03-16 16:58:48 +0100

    timer
    timer

--- main.c
+++ main.c
@@ -29,6 +29,9 @@ int main (int argc, char ** argv)
 	SDL_Texture *texture_btn_option = NULL;
 	SDL_Texture *texture_btn_tuto = NULL;
 
+	Timer_t *temps_jeu;
+	temps_jeu = timer_init();
+
 	SDL_bool program_launched = SDL_TRUE;
 
 	int status_tuto = -1;
@@ -62,7 +65,7 @@ int main (int argc, char ** argv)
 						SDL_DestroyTexture(texture_menu);
 						SDL_DestroyTexture(texture_btn_tuto);
 
-						lancement(renderer, window);
+						lancement(renderer, window, temps_jeu);
 
 						ajout_texture(texture_menu ,"images/menu.jpg" , renderer, window, HAUTEUR , LARGEUR);
 						ajout_texture(texture_btn_jouer ,"images/jouer.bmp" , renderer, window, HAUTEUR , LARGEUR);
