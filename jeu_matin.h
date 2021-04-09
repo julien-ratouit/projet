0869a34a268a4b766b513c67c2754f9f723a088a
Gugulicorne <guiguiv61.270400@gmail.com>
2021-04-09 14:59:19 +0200

    Ez

--- jeu_matin.h
+++ jeu_matin.h
@@ -3,6 +3,6 @@
 
 extern void *fonc_pthread_timer1_cb(void *parametre);
 extern void fonc_pthread_timer1(param_t *parametre);
-extern void lancement_matin(SDL_Renderer *renderer, SDL_Window *window, Timer_t * temps_jeu, int argent);
+extern void lancement_matin(param_t * parametre);
 
 #endif
--- main.c
+++ main.c
@@ -1,7 +1,29 @@
+/**
+* \file main.c
+* \brief fonction principal du programme.
+* \details c'est dans cette fonction que l'on initialise la SDL très utile pour notre projet, on initialise aussi la fenêtre et le rendu pour le reste de notre programme.
+* \author Quenault Maxime, Ratouit Charles
+*/
+
 #include "commun.h"
 #include "jeu_matin.h"
+#include "jeu_apremidi.h"
 #include "tuto.h"
-
+#include "timer.h"
+#include "barre.h"
+#include "self.h"
+#include "salle_prof.h"
+#include "action.h"
+/**
+* \brief fonction main.
+*
+* \details Dans cette fonction le joueur à trois choix, soit commencer une nouvelle partie, soit reprendre sont anciènne sauvegarde si il y en a une, et pour finir il peut ausi
+* acceder à une page "tuto" qui presente les specificité du jeu et donne des tips pour bien jouer.
+* 
+* \param temps_jeu est le timer de notre partie, on l'utilise dans le cours du matin et de l'après-midi.
+* \param program_lauched est de type \a SDL_bool et permet de maintenir le programme en route, si jamais il passe à false alors on quitte le programme.
+*  
+*/
 
 int main (int argc, char ** argv)
 {
@@ -37,6 +59,14 @@ int main (int argc, char ** argv)
 	ajout_texture(texture_btn_tuto ,"images/tuto.png" , renderer, window, HAUTEUR*1.3, LARGEUR);
 	ajout_texture(texture_logo ,"images/logo.png" , renderer, window, HAUTEUR/2 , LARGEUR);
 
+	int quitte = 0;
+	int perdu = 0;
+
+	param_t * parametre = malloc(sizeof(param_t));
+	parametre->temps_jeu = temps_jeu;
+	parametre->argent = argent;
+	parametre->window = window;
+	parametre->renderer = renderer;
 
 	SDL_RenderPresent(renderer);
 	while(program_launched)
@@ -51,13 +81,23 @@ int main (int argc, char ** argv)
 					printf("x : %i\ny : %i\n\n", event.button.x, event.button.y);
 					if((event.button.x < BOUTON_PLAY_X_MAX && event.button.x > BOUTON_PLAY_X_MIN)&&(event.button.y < BOUTON_PLAY_Y_MAX && event.button.y > BOUTON_PLAY_Y_MIN) && status_tuto == -1)
 					{
-						//si on appuie sur le bouton jouer
-
 						SDL_DestroyTexture(texture_btn_jouer);
 						SDL_DestroyTexture(texture_menu);
 						SDL_DestroyTexture(texture_btn_tuto);
 
-						lancement_matin(renderer, window, temps_jeu, argent);
+						while(!quitte && !perdu){
+
+							if(!quitte && !perdu)
+								lancement_matin(parametre);
+							if(!quitte && !perdu)
+								lancement_self(parametre);
+							if(!quitte && !perdu)
+								lancement_apremidi(parametre);
+							if(!quitte && !perdu)
+								lancement_salle_prof(parametre);
+
+						}
+						//si on appuie sur le bouton jouer				
 
 						ajout_texture(texture_menu ,"images/menu.jpg" , renderer, window, HAUTEUR , LARGEUR);
 						ajout_texture(texture_btn_jouer ,"images/jouer.png" , renderer, window, HAUTEUR , LARGEUR);
