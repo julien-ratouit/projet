6f530d8fb9060c5a9c35f491438d199b75534d05
Maxime-Quenault <maximeq72@gmail.com>
2021-04-12 15:58:04 +0200

    correction du menu des actions

--- barre.c
+++ barre.c
@@ -12,9 +12,9 @@
 
 /**
 * \brief Initialise la barre sonore
-*
+* 
 * \details Cette fonction va afficher un rectangle vide à l'ecran, on aura seulement 5px de couleur à l'initialisation.
-*
+*  
 * \param (*barre).x est le point de coordonnée de départ pour les x.
 * \param (*barre).y est le point de coordonnée de départ pour les y.
 * \param (*barre).w est la largeur du rectangle coloré.
@@ -40,9 +40,9 @@ extern void init_barre_sonore(SDL_Renderer *renderer, SDL_Rect *barre){
 
 /**
 * \brief Initialise la barre de depression
-*
+* 
 * \details Cette fonction va afficher un rectangle vide à l'ecran, on aura seulement 5px de couleur à l'initialisation.
-*
+*  
 * \param (*barre).x est le point de coordonnée de départ pour les x.
 * \param (*barre).y est le point de coordonnée de départ pour les y.
 * \param (*barre).w est la largeur du rectangle coloré.
@@ -68,9 +68,9 @@ extern void init_barre_depression(SDL_Renderer *renderer, SDL_Rect *barre){
 
 /**
 * \brief Modifie la barre sonore
-*
+* 
 * \details Cette fonction va augmenter la hauteur du rectangle coloré en prenant comme paramètre un score qui agit sur la valeur d'augmentation.
-*
+*  
 * \param tempo
 * \param score
 */
@@ -128,59 +128,28 @@ extern void update_barre_sonore(SDL_Renderer *renderer, SDL_Rect *barre, int sco
 
 /**
 * \brief Modifie la barre de depression
-*
+* 
 * \details Cette fonction va augmenter la hauteur du rectangle coloré en prenant comme paramètre un score qui agit sur la valeur d'augmentation.
-*
+*  
 * \param tempo
 * \param score
 */
 extern void update_barre_depression(SDL_Renderer *renderer, SDL_Rect *barre_depression, SDL_Rect *barre_sonore, int score){
-  int tempo = 0;
 
   //printf("Score de la barre de depression : %i et score : %i\n", (*barre_depression).h, score);
-  if((*barre_sonore).h == -248 && ((*barre_depression).h-score/2) > -248 && score > 0){
+  if((*barre_sonore).h == -248 && ((*barre_depression).h-score/2) > -248){
     (*barre_depression).h = (*barre_depression).h - score/2;
   }
-  else if(((*barre_depression).h-score/2) <= -248 && score > 0){
+  else if(((*barre_depression).h-score/2) < -248){
     (*barre_depression).h = -248;
-    printf("DEPRESSION BARRE IS FULL\n");
-  }
-  else if(score < 0){
-    (*barre_depression).y = (*barre_depression).y + (*barre_depression).h;
-    if((*barre_depression).h-score >= -5){
-      (*barre_depression).h = 264 - (*barre_depression).y - 5;
-      //printf("VIDE\n");
-    }
-    else{
-      tempo = (*barre_depression).h;
-      (*barre_depression).h = -score;
-      //printf("DECREMENTATION et tempo = %i et h= %i\n",tempo ,(*barre).h);
-    }
+    //printf("DEPRESSION BARRE IS FULL\n");
   }
 
-  if(score >= 0 || (*barre_depression).h == -5){
-    if(SDL_SetRenderDrawColor(renderer, 240, 130, 5, SDL_ALPHA_OPAQUE) != 0)
-      SDL_ExitWithError("Impossible de changer la couleur du rendu");
-  }
-  else{
-    if(SDL_SetRenderDrawColor(renderer, 153, 147, 137, SDL_ALPHA_OPAQUE) != 0)
-      SDL_ExitWithError("Impossible de changer la couleur du rendu");
-  }
+  if(SDL_SetRenderDrawColor(renderer, 240, 130, 5, SDL_ALPHA_OPAQUE) != 0)
+    SDL_ExitWithError("Impossible de changer la couleur du rendu");
 
   if(SDL_RenderFillRect(renderer, barre_depression) != 0)//Pour dessiner un rectangle remplit
     SDL_ExitWithError("Impossible de dessiner un rectangle");
 
   SDL_RenderPresent(renderer);
-
-  if(score < 0 ){
-    if(tempo-score >= -5){
-      (*barre_depression).h = -5;
-    }
-    else{
-      //printf("test tempo score\n");
-      (*barre_depression).h = tempo - score;
-    }
-    (*barre_depression).y = 264;
-  }
-  printf("%i\n",(*barre_depression).h);
 }
