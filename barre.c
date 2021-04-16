cb33ddb8180fd3a2f7555d6c9fe6a0f31200f43b
Maxime-Quenault <maximeq72@gmail.com>
2021-04-16 22:54:25 +0200

    Correction bug de la barre depression
    la barre de depression avait un bug, on l'a corrigé

--- barre.c
+++ barre.c
@@ -134,21 +134,46 @@ extern void update_barre_sonore(SDL_Renderer *renderer, SDL_Rect *barre, int sco
 * \param score
 */
 extern void update_barre_depression(SDL_Renderer *renderer, SDL_Rect *barre_depression, SDL_Rect *barre_sonore, int score){
+  int tempo = 0;
 
-  //printf("Score de la barre de depression : %i et score : %i\n", (*barre_depression).h, score);
-  if((*barre_sonore).h == -248 && ((*barre_depression).h-score/2) > -248){
+  if((*barre_sonore).h == -248 && ((*barre_depression).h-score/2) > -248 && score > 0){
     (*barre_depression).h = (*barre_depression).h - score/2;
   }
-  else if(((*barre_depression).h-score/2) < -248){
+  else if(((*barre_depression).h-score/2) <= -248 && score > 0){
     (*barre_depression).h = -248;
-    //printf("DEPRESSION BARRE IS FULL\n");
+  }
+  else if(score < 0){
+    (*barre_depression).y = (*barre_depression).y + (*barre_depression).h;
+    if((*barre_depression).h-score >= -5){
+      (*barre_depression).h = 264 - (*barre_depression).y - 5;
+    }
+    else{
+      tempo = (*barre_depression).h;
+      (*barre_depression).h = -score;
+    }
   }
 
-  if(SDL_SetRenderDrawColor(renderer, 240, 130, 5, SDL_ALPHA_OPAQUE) != 0)
-    SDL_ExitWithError("Impossible de changer la couleur du rendu");
+  if(score >= 0 || (*barre_depression).h == -5){
+    if(SDL_SetRenderDrawColor(renderer, 240, 130, 5, SDL_ALPHA_OPAQUE) != 0)
+      SDL_ExitWithError("Impossible de changer la couleur du rendu");
+  }
+  else{
+    if(SDL_SetRenderDrawColor(renderer, 153, 147, 137, SDL_ALPHA_OPAQUE) != 0)
+      SDL_ExitWithError("Impossible de changer la couleur du rendu");
+  }
 
   if(SDL_RenderFillRect(renderer, barre_depression) != 0)//Pour dessiner un rectangle remplit
     SDL_ExitWithError("Impossible de dessiner un rectangle");
 
   SDL_RenderPresent(renderer);
+
+  if(score < 0 ){
+    if(tempo-score >= -5){
+      (*barre_depression).h = -5;
+    }
+    else{
+      (*barre_depression).h = tempo - score;
+    }
+    (*barre_depression).y = 264;
+  }
 }
