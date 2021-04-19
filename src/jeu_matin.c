a55ee2784f9ac75d2a297902064452ce5f85e83d
Maxime-Quenault <maximeq72@gmail.com>
2021-04-19 19:25:25 +0200

    reduction de la vitesse des barres

--- src/jeu_matin.c
+++ src/jeu_matin.c
@@ -12,8 +12,8 @@
 #include "../include/self.h"
 #include <time.h>
 
-#define VITESSE_BARRES 3
-#define TEMPS_COUR 6000
+#define VITESSE_BARRES 2
+#define TEMPS_COUR 60000
 
 /**
 * \fn void lancement_matin(param_t * parametre)
